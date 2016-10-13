e9e6e3656ba9a90472ddea84f84b4a28d10d8bb4
catbaron- <catbaron@live.cn>
2016-10-13 16:51:47 +0900

    Update: Search for podcast information

--- g-cores.js
+++ g-cores.js
@@ -1,26 +1,35 @@
 // Copyright (c) 2016 Catbaron. All rights reserved.
 
 url_gcores = "http://www.g-cores.com";
-url_check_msg = url_gcores+'/setting/notifications?ajax=1';
-url_notifications = url_gcores+"/setting/notifications";
-url_inbox = url_gcores+"/inbox";
+// url_check_msg = url_gcores+'/setting/notifications?ajax=1';
+// url_notifications = url_gcores+"/setting/notifications";
+// url_inbox = url_gcores+"/inbox";
 
+// information of podcasts
+var podcast_info = JSON.parse("{}");
+
+// Number of notifications
 var notification_size = mail_size = 0;
 
 // Return the URL used for ajax checking notifications
 function getUrlNotificationsAjax(){
-  return url_check_msg;
+  return url_gcores+'/setting/notifications?ajax=1';;
+}
+
+// Return the URL of podcast page
+function getUrlOfPodcastPage(id){
+  return url_gcores + "volumes/" + id;
 }
 
 // Return the URL of the notification page
 function getUrlNotificationsPage(){
   if(mail_size != 0){
     // if there is a direct message, go to page of inbox
-    return url_inbox;
+    return url_gcores + "/inbox";
   }
   else if(notification_size != 0){
     // otherwise go to the page of notifications
-    return url_notifications;
+    return url_gcores+"/setting/notifications";
   }
   else{
     return url_gcores;
@@ -39,11 +48,11 @@ function checkMSG(url, checkMSGCallback){
 
   xhr.onreadystatechange = function(){
     if(xhr.readyState == 4){
-      callback(xhr, true);
+      checkMSGCallback(xhr, true);
     }
   }
   xhr.onerror = function(){
-    callback(xhr, false);
+    checkMSGCallback(xhr, false);
   }
   xhr.send();
 }
@@ -79,23 +88,20 @@ function checkMSGCallback(xhr, success){
   chrome.browserAction.setBadgeText({text: msg_number});
 }
 
-// Click the button and jump to the notification page.
-// The page will rederict to login page if the user is not login
-// chrome.browserAction.onClicked.addListener(function(){
-//   chrome.tabs.create({url: getUrlNotificationsPage()});
-// });
-
 // Given the url of play pae of one podcast, this function will collect
 // information including:
-//  introduction : [{image_url, text}]
-//  time segment: []
+//  slides : [{image_url, intro_text}]
+//  time segments: []
 //  url of audio file
-function collectInfomation(url){
-  intro = new Array();
-  time_segment = new Array();
-
+function collectInfomation(id){
+  url = "http://www.g-cores.com/volumes/"+id;
+  slides = new Array();
+  time_segments = new Array();
+  // console.log("read "+url);
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", url);
+  xhr.open("GET", url, true);
+  xhr.setRequestHeader("X-Requested-With","XMLHttpRequest");
+  xhr.setRequestHeader("Accept","application/xml, text/javascript, */*; q=0.01");
   xhr.onreadystatechange = function(){
     if(xhr.readyState == 4){
       // Get the html page, collect the information
@@ -103,7 +109,14 @@ function collectInfomation(url){
       //    The first two intro slides are the same to the last two
       //    There is no '0' time segment
       // The real first slide (slide[1]) has no intro text
-      doc = xhr.responseXML;
+      doc = xhr.response.split("new AudioPlayer(")[1].split(/\);\s*}\s*\)\s*<\/script>/)[0];
+      // doc = doc.split(/jplayerSwf: '\/jplayer.swf',/)[0];
+      doc = doc.replace("mediaSrc","\"mediaSrc\"").replace("timelines","\"timelines\"");
+      doc = doc.replace("jplayerSwf","\"jplayerSwf\"").replace("audioId","\"audioId\"");
+      doc = doc.replace("'/jplayer.swf'","\"/jplayer.swf\"");
+      // console.log(doc);
+      podcast_info.id = JSON.parse(doc);
+      // console.log(podcast_info[id]);
     }else{
       // Show the error message
     }
@@ -111,17 +124,22 @@ function collectInfomation(url){
   xhr.onerror = function(){
     // Show the error message
   }
+  xhr.send();
 }
-
 // Receive the request message from the popup page
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-  if(message == 'NumberOfNotification'){
+  if(message.msg == 'GetNumberOfNotification'){
     // Request for the number of notifications
     response = {'notification_size':notification_size, 'mail_size':mail_size}
-    sendResponse(response);
   }
+  if(message.msg == 'GetPodcastInfomation'){
+    response = podcast_info.id;
+  }
+  sendResponse(response);
 });
-
+// collectInfomation("20764");
 setInterval(function(){
-  checkMSG(getUrlNotificationsAjax(), checkMSGCallback)
+  // console.log("23333");
+  checkMSG(getUrlNotificationsAjax(), checkMSGCallback);
+  collectInfomation("20764");
 }, 5000)
--- popup.js
+++ popup.js
@@ -1,9 +1,22 @@
 // Copyright (c) 2016 Catbaron. All rights reserved.
-id_button_notification = "notification"
-id_button_mail = "mail"
+id_button_notification = "notification";
+id_button_mail = "mail";
+id_play_list = "play_list";
 
-function NumberOfNotification(response){
+var podcast_info = new Array();
+
+function getNumberOfNotification(response){
+  // console.log(response);
   document.getElementById(id_button_notification).innerText = response.notification_size;
   document.getElementById(id_button_mail).innerText = response.mail_size;
 }
-chrome.runtime.sendMessage('NumberOfNotification', NumberOfNotification);
+function getPodcastInformation(response){
+  console.log(response);
+  document.getElementById(id_play_list).innerText = JSON.stringify(response);
+  // document.getElementById(id_button_mail).innerText = response.mail_size;
+}
+chrome.runtime.sendMessage({'msg':'GetNumberOfNotification'}, getNumberOfNotification);
+chrome.runtime.sendMessage({'msg':'GetPodcastInfomation', 'data':'20764'}, getPodcastInformation);
+
+//
+// collectInfomation("20764");
